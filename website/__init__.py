f26029f4e00cf4da973f4bb645e0cee5ac2eceff
hijklmno <watson2272003@hotmail.com>
2015-03-14 18:30:47 -0700

    nav split with section def

--- website/__init__.py
+++ website/__init__.py
@@ -15,10 +15,28 @@ app.config['SECRET_KEY'] = 'you-will-never-guess'
 
 app.swgi_app = ProxyFix(app.wsgi_app)
 
+##################
+# Error Handlers #
+##################
+@app.errorhandler(404)
+def page_not_found(error):
+    return redirect(url_for('index')), 404
+
 ##############################
 # Start website redirections #
 ##############################
-@app.route('/', methods = ['GET', 'POST'])
+@app.route('/')
 def index():
 	return render_template('index.html')
 
+@app.route('/1800s')
+def eighteen():
+	return render_template('1800s.html')
+
+@app.route('/1900s')
+def nineteen():
+	return render_template('1900s.html')
+
+@app.route('/2000s')
+def twenty():
+	return render_template('2000s.html')
